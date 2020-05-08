0c4efab5f598c0d141bf24c70a6678e573b2ad45
k4be <k4be@k4be.pl>
2020-05-08 20:14:11 +0200

    Drop mysql support from the wwwstats module. (Another module providing this functionality will be released later - it's already documented.) Also update it to work with 5.0.5.

--- files/wwwstats.c
+++ files/wwwstats.c
@@ -1,11 +1,3 @@
-
- /* for compile, use:
- EXLIBS="-lmysqlclient" make
- */
-
-#undef USE_MYSQL
-// change #undef to #define to enable mysql support
-
 /* Copyright (C) All Rights Reserved
 ** Written by rocket & k4be
 ** Website: https://github.com/pirc-pl/unrealircd-modules/
@@ -21,39 +13,22 @@ module
         post-install-text {
                 "The module is installed. Now you need to add a loadmodule line:";
                 "loadmodule \"third/wwwstats\";";
-                "and create a valid configuration block as in the example below:";
+                "then create a valid configuration block as in the example below:";
                 "wwwstats {";
 				" socket-path \"/tmp/wwwstats.sock\"; // do not specify if you don't want the socket";
-				" use-mysql; // remove this line if you don't want mysql";
-				" mysql-interval \"900\"; // time in seconds, default is 900";
-				" mysql-host \"localhost\";";
-				" mysql-db \"database\";";
-				" mysql-user \"username\";";
-				" mysql-pass \"password\";";
 				"};";
 				"And /REHASH the IRCd.";
 				"";
-				"Please note that due to the limitations of Unreal's module manager, the MySQL support";
-				"is disabled by default. To enable it, first make sure that you have a MySQL client library";
-				"installed on your system. Next, please edit the file \"src/modules/third/wwwstats.c\"";
-				"and change the line \"#undef USE_MYSQL\" to \"#define USE_MYSQL\".";
-				"Then rebuild the module running:";
-				"EXLIBS=\"-lmysqlclient\" make install";
-				"After /REHASH ing the MySQL support will be active.";
-				"Detailed documentation is available on https://github.com/pirc-pl/unrealircd-modules/blob/master/README.md";
+				"If you want a version with MySQL/MariaDB support, look for 'wwwstats-mysql'";
+				"on https://github.com/pirc-pl/unrealircd-modules - unfortunately it can't";
+				"be installed by the module manager.";
+				"Detailed documentation is available on https://github.com/pirc-pl/unrealircd-modules/blob/master/README.md#wwwstats";
         }
 }
 *** <<<MODULE MANAGER END>>>
 */
 
 #define MYCONF "wwwstats"
-
-#ifdef USE_MYSQL
-#define DEFAULT_MYSQL_INTERVAL 900
-#define list_add list_add_MYSQL
-#include <mysql/mysql.h>
-#undef list_add
-#endif
 
 #include "unrealircd.h"
 #include <sys/socket.h>
@@ -62,6 +37,12 @@ module
 
 #ifndef TOPICLEN
 #define TOPICLEN MAXTOPICLEN
+#endif
+
+#if (UNREAL_VERSION_GENERATION == 5 && UNREAL_VERSION_MAJOR == 0 && UNREAL_VERSION_MINOR < 5)
+#define MESSAGE_SENDTYPE int
+#else
+#define MESSAGE_SENDTYPE SendType
 #endif
 
 struct chanStats_s {
@@ -95,11 +76,8 @@ time_t init_time;
 int stats_socket;
 char send_buf[4096];
 struct sockaddr_un stats_addr;
-#ifdef USE_MYSQL
-MYSQL *stats_db;
-#endif
 
-int wwwstats_msg(Client *sptr, Channel *chptr, MessageTag *mtags, char *msg, int notice);
+int wwwstats_msg(Client *sptr, Channel *chptr, MessageTag *mtags, char *msg, MESSAGE_SENDTYPE sendtype);
 EVENT(wwwstats_socket_evt);
 void asend_sprintf(asendInfo *info, char *fmt, ...);
 void append_int_param(asendInfo *info, char *param, int value);
@@ -110,15 +88,6 @@ char *tmp_escape(char *d, const char *a);
 char *json_escape(char *d, const char *a);
 void appendChannel(Channel *ch, int messages);
 
-#ifdef USE_MYSQL
-void saveChannels(time_t act_time);
-void saveStats(time_t act_time);
-int mysql_query_sprintf(char *buf, char *fmt, ...);
-void loadChannels(void);
-void send_mysql_error(void);
-EVENT(wwwstats_mysql_evt);
-#endif
-
 int wwwstats_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs);
 int wwwstats_configposttest(int *errs);
 int wwwstats_configrun(ConfigFile *cf, ConfigEntry *ce, int type);
@@ -127,14 +96,6 @@ chanStats *chans, *chans_last;
 
 // config file stuff, based on Gottem's module
 
-#ifdef USE_MYSQL
-static char *mysql_user;
-static char *mysql_pass;
-static char *mysql_db;
-static char *mysql_host;
-static int use_mysql;
-static int mysql_interval;
-#endif
 static char *socket_path;
 int socket_hpath=0;
 
@@ -142,10 +103,6 @@ int wwwstats_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs) {
 	ConfigEntry *cep; // For looping through our bl0cc
 	int errors = 0; // Error count
 	int i; // iter8or m8
-	
-#ifdef USE_MYSQL
-	int mysql_huser=0, mysql_hpass=0, mysql_hdb=0, mysql_en=0, mysql_hhost=0;
-#endif
 
 	// Since we'll add a new top-level block to unrealircd.conf, need to filter on CONFIG_MAIN lmao
 	if(type != CONFIG_MAIN)
@@ -168,74 +125,6 @@ int wwwstats_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs) {
 			continue; // Next iteration imo tbh
 		}
 
-#ifdef USE_MYSQL
-		if(!strcmp(cep->ce_varname, "mysql-user")) {
-			if(!cep->ce_vardata) {
-				config_error("%s:%i: %s::%s must be a string", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-				continue;
-			}
-			mysql_huser=1;
-			continue;
-		}
-
-		if(!strcmp(cep->ce_varname, "mysql-pass")) {
-			if(!cep->ce_vardata) {
-				config_error("%s:%i: %s::%s must be a string", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-				continue;
-			}
-			mysql_hpass=1;
-			continue;
-		}
-		
-		if(!strcmp(cep->ce_varname, "mysql-db")) {
-			if(!cep->ce_vardata) {
-				config_error("%s:%i: %s::%s must be a string", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-				continue;
-			}
-			mysql_hdb=1;
-			continue;
-		}
-		
-		if(!strcmp(cep->ce_varname, "mysql-host")) {
-			if(!cep->ce_vardata) {
-				config_error("%s:%i: %s::%s must be a string", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-				continue;
-			}
-			mysql_hhost=1;
-			continue;
-		}
-		
-		if(!strcmp(cep->ce_varname, "mysql-interval")) {
-			if(!cep->ce_vardata) {
-				config_error("%s:%i: %s::%s must be an integer between 1 and 1000 (minutes)", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-				continue; // Next iteration imo tbh
-			}
-			// Should be an integer yo
-			for(i = 0; cep->ce_vardata[i]; i++) {
-				if(!isdigit(cep->ce_vardata[i])) {
-					config_error("%s:%i: %s::%s must be an integer between 1 and 1000 (minutes)", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-					errors++; // Increment err0r count fam
-					break;
-				}
-			}
-			if(!errors && (atoi(cep->ce_vardata) < 1 || atoi(cep->ce_vardata) > 1000)) {
-				config_error("%s:%i: %s::%s must be an integer between 1 and 1000 (minutes)", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
-				errors++; // Increment err0r count fam
-			}
-			continue;
-		}
-
-		if(!strcmp(cep->ce_varname, "use-mysql")) { // no value expected
-			mysql_en = 1;
-			continue;
-		}
-#endif
-
 		if(!strcmp(cep->ce_varname, "socket-path")) {
 			if(!cep->ce_vardata) {
 				config_error("%s:%i: %s::%s must be a path", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname);
@@ -250,13 +139,6 @@ int wwwstats_configtest(ConfigFile *cf, ConfigEntry *ce, int type, int *errs) {
 		config_warn("%s:%i: unknown item %s::%s", cep->ce_fileptr->cf_filename, cep->ce_varlinenum, MYCONF, cep->ce_varname); // So display just a warning
 	}
 
-#ifdef USE_MYSQL
-	if(mysql_en && (!mysql_huser || !mysql_hpass || !mysql_hdb || !mysql_hhost)){
-		config_warn("m_wwwstats: error: your mysql configuration is incomplete! Please either correct or disable it!");
-		errors++;
-	}
-#endif
-	
 	*errs = errors;
 	return errors ? -1 : 1; // Returning 1 means "all good", -1 means we shat our panties
 }
@@ -290,46 +172,11 @@ int wwwstats_configrun(ConfigFile *cf, ConfigEntry *ce, int type) {
 		if(!cep->ce_varname)
 			continue; // Next iteration imo tbh
 
-#ifdef USE_MYSQL
-		if(cep->ce_vardata && !strcmp(cep->ce_varname, "mysql-user")) {
-			mysql_user = strdup(cep->ce_vardata);
-			continue;
-		}
-		
-		if(cep->ce_vardata && !strcmp(cep->ce_varname, "mysql-pass")) {
-			mysql_pass = strdup(cep->ce_vardata);
-			continue;
-		}
-		
-		if(cep->ce_vardata && !strcmp(cep->ce_varname, "mysql-db")) {
-			mysql_db = strdup(cep->ce_vardata);
-			continue;
-		}
-
-		if(cep->ce_vardata && !strcmp(cep->ce_varname, "mysql-host")) {
-			mysql_host = strdup(cep->ce_vardata);
-			continue;
-		}
-		
-		if(!strcmp(cep->ce_varname, "mysql-interval")) {
-			mysql_interval = atoi(cep->ce_vardata);
-			continue;
-		}
-		
-		if(!strcmp(cep->ce_varname, "use-mysql")) {
-			use_mysql = 1;
-			continue;
-		}
-#endif
-		
 		if(cep->ce_vardata && !strcmp(cep->ce_varname, "socket-path")) {
 			socket_path = strdup(cep->ce_vardata);
 			continue;
 		}
 	}
-#ifdef USE_MYSQL
-	if(mysql_interval == 0) mysql_interval = DEFAULT_MYSQL_INTERVAL;
-#endif
 	return 1; // We good
 }
 
@@ -363,22 +210,12 @@ MOD_INIT(){
 
 /* Is first run when server is 100% ready */
 MOD_LOAD(){
-	#ifdef USE_MYSQL
-	MYSQL_RES *res;
-	MYSQL_ROW row;
-	#endif
-	
 	if(socket_path){
 		stats_addr.sun_family = AF_UNIX;
 		strcpy(stats_addr.sun_path, socket_path);
 		unlink(stats_addr.sun_path);	// remove old socket if exists
 	}
 
-	#ifdef USE_MYSQL
-	stats_db = mysql_init(NULL);
-	if(!stats_db) send_mysql_error(); // init failed
-	#endif
-	
 	counter = 0;
 
 	chans = NULL;
@@ -392,30 +229,7 @@ MOD_LOAD(){
 		fcntl(stats_socket, F_SETFL, O_NONBLOCK);
 	}
 
-	#ifdef USE_MYSQL
-	if(use_mysql && mysql_host && mysql_user && mysql_pass && mysql_db){ // initialize DB
-		mysql_real_connect(stats_db, mysql_host, mysql_user, mysql_pass, mysql_db, 0, NULL, 0);
-
-		mysql_query(stats_db, "CREATE TABLE IF NOT EXISTS `chanlist` (`id` int(11) NOT NULL AUTO_INCREMENT, `date` int(11), `name` char(64), `topic` text, `users` int(11),  `messages` int(11), PRIMARY KEY (`id`), UNIQUE KEY `name` (`name`,`users`,`messages`), KEY `name_3` (`name`), KEY `date` (`date`) )");
-		mysql_query(stats_db, "CREATE TABLE IF NOT EXISTS `stat` (`id` int(11) NOT NULL AUTO_INCREMENT, `date` int(11), `clients` int(11), `servers` int(11), `messages` int(11), `channels` int(11), PRIMARY KEY (`id`), UNIQUE KEY `changes` (`clients`,`servers`,`messages`,`channels`), KEY `date` (`date`) )");
-
-		mysql_query(stats_db, "SELECT messages FROM stat ORDER BY id DESC LIMIT 1"); // read old messages count to continue counting
-		res = mysql_use_result(stats_db);
-		if(!res) send_mysql_error(); else {
-			if((row = mysql_fetch_row(res))) {
-				counter = strtoul(row[0], NULL, 10);
-			}
-			mysql_free_result(res);
-		}
-
-		loadChannels();
-	}
-
-	#endif
 	EventAdd(modinfo->handle, "wwwstats_socket", wwwstats_socket_evt, NULL, 100, 0);
-	#ifdef USE_MYSQL
-	EventAdd(modinfo->handle, "wwwstats_mysql", wwwstats_mysql_evt, NULL, mysql_interval*1000, 0);
-	#endif
 
 	return MOD_SUCCESS;
 }
@@ -430,36 +244,18 @@ MOD_UNLOAD(){
 
 	act_time = time(NULL);
 
-	#ifdef USE_MYSQL
-	saveStats(act_time);
-	saveChannels(act_time);
-	#endif
-
 	for(;chans;chans=next) {
 		next=chans->next;
 		free(chans);
 	}
-	#ifdef USE_MYSQL
-	if(stats_db) mysql_close(stats_db); // free our strings
-	if(mysql_user) free(mysql_user);
-	if(mysql_pass) free(mysql_pass);
-	if(mysql_db) free(mysql_db);
-	if(mysql_host) free(mysql_host);
-	#endif
 
 	if(socket_path) free(socket_path);
 	
 	return MOD_SUCCESS;
 }
 
-int wwwstats_msg(Client *sptr, Channel *chptr, MessageTag *mtags, char *msg, int notice) { // called on channel messages
+int wwwstats_msg(Client *sptr, Channel *chptr, MessageTag *mtags, char *msg, MESSAGE_SENDTYPE sendtype) { // called on channel messages
 	chanStats *lp;
-	#ifdef USE_MYSQL
-	char name[2*CHANNELLEN+1];
-	char buf[2048];
-	MYSQL_RES *res;
-	MYSQL_ROW row;
-	#endif
 	int c_msg;
 	counter++;
 	for(lp=chans; lp; lp=lp->next) if(lp->chan==chptr) break;
@@ -467,18 +263,6 @@ int wwwstats_msg(Client *sptr, Channel *chptr, MessageTag *mtags, char *msg, int
 	if(lp) lp->msg++; // if channel found, increase msg count
 	else { // create new channel
 		c_msg = 1;
-		#ifdef USE_MYSQL
-		if(use_mysql){
-			tmp_escape(name, chptr->chname);
-			mysql_query_sprintf(buf, "SELECT MAX(messages) FROM chanlist WHERE name=\"%s\" GROUP BY name", name);
-			res = mysql_use_result(stats_db);
-			if(!res) send_mysql_error(); else {
-				if((row = mysql_fetch_row(res)))
-					if(row[0]) c_msg = strtoul(row[0], NULL, 10);
-				mysql_free_result(res);
-			}
-		}
-		#endif
 //	    sendto_realops("wwwstats: added channel %s, %d msgs", name, c_msg);
 		appendChannel(chptr, c_msg);
 	}
@@ -554,77 +338,6 @@ EVENT(wwwstats_socket_evt){
 
 	close(sock);
 }
-
-#ifdef USE_MYSQL
-EVENT(wwwstats_mysql_evt){
-	time_t act_time;
-	act_time = time(NULL);
-	
-	saveStats(act_time);
-	removeExpiredChannels();
-	saveChannels(act_time);
-}
-
-void saveChannels(time_t act_time) { // store all channel data to DB
-	char buf[6*(TOPICLEN+CHANNELLEN)+256];
-	char name[6*CHANNELLEN+1];
-	char topic[6*TOPICLEN+1];
-	channelInfo chinfo;
-	
-	if(!use_mysql) return;
-
-	chinfo.chan = NULL;
-	while(getChannelInfo(&chinfo)) {
-		json_escape(name, chinfo.chan->chname);
-		if(chinfo.chan->topic) json_escape(topic, chinfo.chan->topic);
-		else topic[0] = 0;
-		mysql_query_sprintf(buf, "INSERT IGNORE INTO chanlist VALUES (NULL, %d, \"%s\", \"%s\", %d, %d)", act_time, name, topic, chinfo.chan->users, chinfo.messages);
-	}
-}
-
-void loadChannels(void){ // channel will be added automatically when a message comes
-	char buf[2*CHANNELLEN+20];
-	char name[2*CHANNELLEN+1];
-	Channel *ch;
-	MYSQL_RES *res;
-	MYSQL_ROW row;
-	
-	channelInfo chinfo;
-	unsigned long cnt;
-	
-	if(!use_mysql) return;
-	
-	chinfo.chan = NULL;
-	while(getChannelInfo(&chinfo)) {
-		ch = chinfo.chan;
-		tmp_escape(name, chinfo.chan->chname);
-		mysql_query_sprintf(buf, "SELECT MAX(messages) FROM chanlist WHERE name = \"%s\"", name);
-		res = mysql_use_result(stats_db);
-		if(res && (row = mysql_fetch_row(res))){
-			cnt = 0;
-			if(row[0]) cnt = strtoul(row[0], NULL, 10);
-			if(cnt > 0){
-				appendChannel(ch, cnt);
-//				sendto_realops("wwwstats: added from db: %s, %lu msgs", ch->chname, cnt);
-			}
-			mysql_free_result(res);
-		}
-	}
-}
-
-void saveStats(time_t act_time) { // store non-channel stats
-	char buf[512];
-	
-	if(!use_mysql) return;
-	
-	mysql_query_sprintf(buf, "INSERT IGNORE INTO stat VALUES (NULL, %d, %d, %d, %d, %d)", act_time, irccounts.clients, irccounts.servers, counter, irccounts.channels);
-}
-
-void send_mysql_error(void){
-	sendto_realops("wwwstats: mysql error: %s",mysql_error(stats_db));
-}
-#endif
-
 
 void appendChannel(Channel *ch, int messages) {
 	chanStats *lp;
@@ -708,21 +421,6 @@ int getChannelInfo(channelInfo *prev) {
 	prev->messages = messages;
 	return 1;
 }
-
-#ifdef USE_MYSQL
-int mysql_query_sprintf(char *buf, char *fmt, ...) {
-	int ret;
-	va_list list;
-	va_start(list, fmt);
-	vsprintf(buf, fmt, list);
-	va_end(list);
-	ret = mysql_query(stats_db, buf);
-	if(ret){
-	      sendto_realops("wwwstats: mysql query error: %s",mysql_error(stats_db));
-	}
-	return ret;
-}
-#endif
 
 void asend_sprintf(asendInfo *info, char *fmt, ...) {
 	int bl, tl;
