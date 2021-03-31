3aab2741b8eb2aa68db824aa90467d6e4d42cac7
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2021-03-31 03:22:05 +0100

    Create chgswhois.c
    This module provides command /CHGSWHOIS which changes a users "special whois" line to the string you specify

---
+++ files/chgswhois.c
@@ -0,0 +1,109 @@
+/*
+This module provides command /CHGSWHOIS which does what it says on the tin
+
+This module changes a users "special whois" line to the string you specify
+
+Syntax: CHGSWHOIS username is a bastardó
+
+This module needs to be loaded on all servers on the network
+
+License: GPLv3
+*/
+/*** <<<MODULE MANAGER START>>>
+module {
+	documentation "https://github.com/ValwareIRC/valware-unrealircd-mods/blob/main/chgswhois/README.md";
+	troubleshooting "In case of problems, check the README or e-mail me at v.a.pond@outlook.com";
+	min-unrealircd-version "5.*";
+	
+	post-install-text {
+		"The module is installed, now all you need to do is add a 'loadmodule' line to your config file:";
+		"loadmodule \"third/CHGSWHOIS\";";
+		"Then /rehash the IRCd.";
+		"This module have no configuration";
+	}
+}
+*** <<<MODULE MANAGER END>>>
+*/
+
+#include "unrealircd.h"
+
+// deFIIIINEs
+#define CHGCMD "CHGSWHOIS"
+#define CheckAPIError(apistr, apiobj) \
+	do { \
+		if(!(apiobj)) { \
+			config_error("A critical error occurred on %s for %s: %s", (apistr), MOD_HEADER.name, ModuleGetErrorStr(modinfo->handle)); \
+			return MOD_FAILED; \
+		} \
+	} while(0)
+		
+	
+CMD_FUNC(CHGSWHOIS);
+
+ModuleHeader MOD_HEADER = {
+	"third/chgswhois",
+	"1.0",
+	"Provides command /CHGSWHOIS for priviledged IRCops to change a users \"special whois\" line.",
+	"Valware",
+	"unrealircd-5",
+};
+
+MOD_INIT() {
+	CheckAPIError("CommandAdd(CHGSWHOIS)", CommandAdd(modinfo->handle, CHGCMD, CHGSWHOIS, 2, CMD_USER));
+	MARK_AS_GLOBAL_MODULE(modinfo);
+	return MOD_SUCCESS;
+}
+
+MOD_LOAD() {
+	return MOD_SUCCESS;
+}
+MOD_UNLOAD() {
+	return MOD_SUCCESS;
+}
+
+CMD_FUNC(CHGSWHOIS) {
+	
+	Client *splooge;
+	char tag[HOSTLEN+1];
+	char swhois[SWHOISLEN+1];
+	*tag = *swhois = '\0';
+	// we're somewhere in there!
+	int priority = 5;
+	
+	if (!ValidatePermissionsForPath("client:set:name",client,NULL,NULL,NULL)) {
+		sendnumeric(client, ERR_NOPRIVILEGES);
+		return;
+	}
+
+	else if ((parc < 3) || !*parv[2]) {
+		sendnumeric(client, ERR_NEEDMOREPARAMS, CHGCMD);
+		return;
+	}
+
+	else if (strlen(parv[2]) > (SWHOISLEN)) {
+		sendnotice(client, "*** CHGSWHOIS rejected: Too long");
+		return;
+	}
+
+	else if (!(splooge = find_person(parv[1], NULL))) {
+		sendnumeric(client, ERR_NOSUCHNICK, parv[1]);
+		return;
+	}
+	strlcpy(tag, client->name, sizeof(tag));
+	strlcpy(swhois, parv[2], sizeof(swhois));
+	sendto_snomask(SNO_EYES,"%s changed the SWHOIS of %s (%s@%s) to be '%s'", client->name, splooge->name, splooge->user->username, GetHost(splooge), parv[2]);
+	/* Logging ability added by XeRXeS */
+	ircd_log(LOG_CHGCMDS, "CHGSWHOIS: %s changed the SWHOIS of %s (%s@%s) to be '%s'", client->name, splooge->name, splooge->user->username, GetHost(splooge), parv[2]);
+	
+	// Find and delete old swhois line
+	SWhois *ours;
+	for (ours = splooge->user->swhois; ours; ours = ours->next)
+		swhois_delete(splooge, ours->setby, ours->line, client, client);
+	
+	// Add our new one!
+	swhois_add(splooge, tag, priority, swhois, client, client);
+	return;
+	
+}
+
+		
