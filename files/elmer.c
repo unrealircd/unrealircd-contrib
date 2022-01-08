95a1157508c3d5e3eeea087b5c6ae076113b913b
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2022-01-08 11:23:03 +0000

    Update elmer.c because I just realised about checks happening on the other side lol (#56)

--- files/elmer.c
+++ files/elmer.c
@@ -112,6 +112,9 @@ CMD_FUNC(ADDELMER)
 {
 	Client *target;
 	
+	if (hunt_server(client, NULL, "ELMER", 1, parc, parv) != HUNTED_ISME)
+		return;
+	
 	if (!IsOper(client))
 	{
 		sendnumeric(client, ERR_NOPRIVILEGES);
@@ -144,6 +147,8 @@ CMD_FUNC(DELELMER)
 {
 	Client *target;
 	
+	if (hunt_server(client, NULL, "DELMER", 1, parc, parv) != HUNTED_ISME)
+		return;
 
 	if (!IsOper(client))
 	{
@@ -163,6 +168,7 @@ CMD_FUNC(DELELMER)
 		sendnumeric(client, ERR_NOPRIVILEGES);
 		return;	
 	}
+	
 	if (!IsElmer(target))
 	{
 		sendnotice(client,"%s was not talking like Elmer anyway.",target->name);
