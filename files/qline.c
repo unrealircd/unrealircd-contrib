d1de63079f73d6f03304f3c6f2a530e81413664c
Avery "Hexick" Q <47760583+Hexsl@users.noreply.github.com>
2023-02-17 09:09:56 -0500

    Updated copyright notice.
    Changed the MIT copyright license to the current year.

--- files/qline.c
+++ files/qline.c
@@ -9,7 +9,7 @@
 * -----------------------------------------------------------------------------------------------------------------------------------------------
 * MIT License
 *
-* Copyright (c) 2022 Avery 'Hexick' Q. [pseudonym]
+* Copyright (c) 2023 Avery 'Hexick' Q. [pseudonym]
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
