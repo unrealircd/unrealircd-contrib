e337238c4763501c97af70c970616a680bc1c737
Valerie Liu <79415174+ValwareIRC@users.noreply.github.com>
2023-10-23 21:32:56 +0100

    Be clear about the URL

--- files/filehost.c
+++ files/filehost.c
@@ -45,7 +45,7 @@ ModuleHeader MOD_HEADER
   = {
 	"third/filehost",
 	"1.0",
-	"IRCv3 draft/FILEHOST - Provides users with an ISUPPORT token about your file upload service",
+	"IRCv3 draft/FILEHOST - Provides users with an ISUPPORT token with a URL to your file upload service",
 	"Valware",
 	"unrealircd-6",
 };
